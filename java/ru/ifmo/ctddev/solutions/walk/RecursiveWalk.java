b76895bf0c721a4f6cd0fee18e7b906b22ecfd69
andrsuh <andrsuh13@yandex.ru>
2017-12-02 21:19:09 +0300

    test commit

--- java/ru/ifmo/ctddev/solutions/walk/RecursiveWalk.java
+++ java/ru/ifmo/ctddev/solutions/walk/RecursiveWalk.java
@@ -4,5 +4,6 @@ public class RecursiveWalk extends HashWalker {
 
     public static void main(String[] args) {
         //todo: use HashWalker methods for calculateHash
+        // todo: test commit
     }
 }
