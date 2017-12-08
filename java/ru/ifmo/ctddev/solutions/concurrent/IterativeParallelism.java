3ff27310185ecf70423bbd03c8be63d50bb4db69
andrsuh <andrsuh13@yandex.ru>
2017-12-08 11:18:33 +0300

    Iterative parallelism initial fixes

--- java/ru/ifmo/ctddev/solutions/concurrent/IterativeParallelism.java
+++ java/ru/ifmo/ctddev/solutions/concurrent/IterativeParallelism.java
@@ -13,19 +13,110 @@ public class IterativeParallelism implements ListIP {
         int taskNum = getTaskNumber(i, list);
         List<T> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
 
+        Function<List<? extends T>, T> f = (l -> Collections.max(l, comparator));
+
+        List<Runnable> tasks = splitAndPrepareTasks(list, taskNum, threadResults, f);
+        runAllTask(tasks);
+
+        return Collections.max(threadResults, comparator);
+    }
+
+    public <T> T minimum(int i, List<? extends T> list, Comparator<? super T> comparator) throws InterruptedException {
+        int taskNum = getTaskNumber(i, list);
+        List<T> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
+
+        Function<List<? extends T>, T> f = l -> Collections.min(l, comparator);
+
+        List<Runnable> tasks = splitAndPrepareTasks(list, taskNum, threadResults, f);
+        runAllTask(tasks);
+
+        return Collections.min(threadResults, comparator);
+    }
+
+    public <T> boolean all(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
+        int taskNum = getTaskNumber(i, list);
+        List<Boolean> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
+
+        Function<List<? extends T>, Boolean> f = l -> l.stream().allMatch(predicate);
+
+        List<Runnable> tasks = splitAndPrepareTasks(list, taskNum, threadResults, f);
+        runAllTask(tasks);
+
+        return threadResults.stream().allMatch(value -> value);
+
+    }
+
+    public <T> boolean any(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
+        int taskNum = getTaskNumber(i, list);
+        List<Boolean> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
+
+        Function<List<? extends T>, Boolean> f = l -> l.stream().anyMatch(predicate);
+
+        List<Runnable> tasks = splitAndPrepareTasks(list, taskNum, threadResults, f);
+        runAllTask(tasks);
+
+        return threadResults.stream().anyMatch(value -> value);
+    }
+
+    @Override
+    public String join(int i, List<?> list) throws InterruptedException {
+        int taskNum = getTaskNumber(i, list);
+        List<String> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
+
+        Function<List<?>, String> f = l -> l.stream().map(Object::toString).collect(Collectors.joining());
+
+        List<Runnable> tasks = splitAndPrepareTasks(list, taskNum, threadResults, f);
+        runAllTask(tasks);
+
+        return threadResults.stream().collect(Collectors.joining());
+    }
+
+    @Override
+    public <T> List<T> filter(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
+        int taskNum = getTaskNumber(i, list);
+        List<List<T>> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
+
+        Function<List<? extends T>, List<T>> f = l -> l.stream().filter(predicate).collect(Collectors.toList());
+
+        List<Runnable> tasks = splitAndPrepareTasks(list, taskNum, threadResults, f);
+        runAllTask(tasks);
+
+        return threadResults.stream().flatMap(Collection::stream).collect(Collectors.toList());
+    }
+
+    @Override
+    public <T, U> List<U> map(int i, List<? extends T> list, Function<? super T, ? extends U> function) throws InterruptedException {
+        int taskNum = getTaskNumber(i, list);
+        List<List<U>> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
+
+        Function<List<? extends T>, List<U>> f = l -> l.stream().map(function).collect(Collectors.toList());
+
+        List<Runnable> tasks = splitAndPrepareTasks(list, taskNum, threadResults, f);
+        runAllTask(tasks);
+
+        return threadResults.stream().flatMap(Collection::stream).collect(Collectors.toList());
+    }
+
+    private <T> int getTaskNumber(int i, List<? extends T> list) {
+        return list.size() < i ? list.size() : i;
+    }
+
+    private <T, R> List<Runnable> splitAndPrepareTasks(List<? extends T> elements, int taskNum,
+                                                       List<R> threadResults, Function<List<? extends T>, R> function) {
         List<Runnable> tasks = new ArrayList<>();
-        int subListLength = list.size() / taskNum;
-        for (i = 0; i < taskNum; i++) {
+
+        int subListLength = elements.size() / taskNum;
+        for (int i = 0; i < taskNum; i++) {
             int task = i;
             tasks.add(() -> {
                 int fromIndex = subListLength * task;
-                int toIndex = task == taskNum - 1 ? list.size() : fromIndex + subListLength;
-                threadResults.set(task, Collections.max(list.subList(fromIndex, toIndex), comparator));
+                int toIndex = task == taskNum - 1 ? elements.size() : fromIndex + subListLength;
+                threadResults.set(task, function.apply(elements.subList(fromIndex, toIndex)));
             });
+
         }
 
-        runAllTask(tasks);
-        return Collections.max(threadResults, comparator);
+        return tasks;
     }
 
     private void runAllTask(List<Runnable> tasks) throws InterruptedException {
@@ -40,145 +131,5 @@ public class IterativeParallelism implements ListIP {
         for (Thread thread : threads) {
             thread.join();
         }
-    }
-
-
-    private <T> int getTaskNumber(int i, List<? extends T> list) {
-        return list.size() < i ? list.size() : i;
-    }
-
-    public <T> T minimum(int i, List<? extends T> list, Comparator<? super T> comparator) throws InterruptedException {
-        int taskNum = getTaskNumber(i, list);
-        List<T> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
-
-
-        List<Runnable> tasks = new ArrayList<>();
-        int subListLength = list.size() / taskNum;
-        for (i = 0; i < taskNum; i++) {
-            int task = i;
-            tasks.add(() -> {
-                int fromIndex = subListLength * task;
-                int toIndex = task == taskNum - 1 ? list.size() : fromIndex + subListLength;
-                T min = list.get(fromIndex);
-                for (int j = fromIndex + 1; j < toIndex; ++j) {
-                    if (comparator.compare(min, list.get(j)) > 0) {
-                        min = list.get(j);
-                    }
-                }
-                threadResults.set(task, min);
-            });
-        }
-
-        runAllTask(tasks);
-        return Collections.min(threadResults, comparator);
-    }
-
-    public <T> boolean all(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
-        int taskNum = getTaskNumber(i, list);
-        List<Boolean> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
-
-        List<Runnable> tasks = new ArrayList<>();
-        int subListLength = list.size() / taskNum;
-        for (i = 0; i < taskNum; i++) {
-            int task = i;
-            tasks.add(() -> {
-                int fromIndex = subListLength * task;
-                int toIndex = task == taskNum - 1 ? list.size() : fromIndex + subListLength;
-                threadResults.set(task, list.subList(fromIndex, toIndex).stream().allMatch(predicate));
-            });
-        }
-
-        runAllTask(tasks);
-        return threadResults.stream().allMatch(value -> value);
-
-    }
-
-    public <T> boolean any(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
-        int taskNum = getTaskNumber(i, list);
-        List<Boolean> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
-
-        List<Runnable> tasks = new ArrayList<>();
-        int subListLength = list.size() / taskNum;
-        for (i = 0; i < taskNum; i++) {
-            int task = i;
-            tasks.add(() -> {
-                int fromIndex = subListLength * task;
-                int toIndex = task == taskNum - 1 ? list.size() : fromIndex + subListLength;
-                threadResults.set(task, list.subList(fromIndex, toIndex).stream().anyMatch(predicate));
-            });
-
-        }
-
-        runAllTask(tasks);
-        return threadResults.stream().anyMatch(value -> value);
-    }
-
-    @Override
-    public String join(int i, List<?> list) throws InterruptedException {
-        int taskNum = getTaskNumber(i, list);
-        List<String> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
-
-        List<Runnable> tasks = new ArrayList<>();
-        int subListLength = list.size() / taskNum;
-        for (i = 0; i < taskNum; i++) {
-            int task = i;
-            tasks.add(() -> {
-                int fromIndex = subListLength * task;
-                int toIndex = task == taskNum - 1 ? list.size() : fromIndex + subListLength;
-                StringBuilder result = new StringBuilder();
-                list.subList(fromIndex, toIndex).forEach(result::append);
-                threadResults.set(task, result.toString());
-            });
-
-        }
-
-        runAllTask(tasks);
-        return threadResults.stream().collect(Collectors.joining());
-    }
-
-    @Override
-    public <T> List<T> filter(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
-        int taskNum = getTaskNumber(i, list);
-        List<List<T>> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
-
-        List<Runnable> tasks = new ArrayList<>();
-        int subListLength = list.size() / taskNum;
-        for (i = 0; i < taskNum; i++) {
-            int task = i;
-            tasks.add(() -> {
-                int fromIndex = subListLength * task;
-                int toIndex = task == taskNum - 1 ? list.size() : fromIndex + subListLength;
-                List<T> result = list.subList(fromIndex, toIndex).stream()
-                        .filter(predicate).collect(Collectors.toList());
-                threadResults.set(task, result);
-            });
-
-        }
-
-        runAllTask(tasks);
-        return threadResults.stream().flatMap(Collection::stream).collect(Collectors.toList());
-    }
-
-    @Override
-    public <T, U> List<U> map(int i, List<? extends T> list, Function<? super T, ? extends U> function) throws InterruptedException {
-        int taskNum = getTaskNumber(i, list);
-        List<List<U>> threadResults = new ArrayList<>(Collections.nCopies(taskNum, null));
-
-        List<Runnable> tasks = new ArrayList<>();
-        int subListLength = list.size() / taskNum;
-        for (i = 0; i < taskNum; i++) {
-            int task = i;
-            tasks.add(() -> {
-                int fromIndex = subListLength * task;
-                int toIndex = task == taskNum - 1 ? list.size() : fromIndex + subListLength;
-                List<U> result = list.subList(fromIndex, toIndex).stream()
-                        .map(function::apply).collect(Collectors.toList());
-                threadResults.set(task, result);
-            });
-
-        }
-
-        runAllTask(tasks);
-        return threadResults.stream().flatMap(Collection::stream).collect(Collectors.toList());
     }
 }
