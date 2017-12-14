5d7c8c9ba2b693cefdac351251c396c415c78091
ol9shvaleva <ol9shvaleva@mail.ru>
2017-12-15 00:38:00 +0300

    implemented lab 3, 6, 7

--- java/ru/ifmo/ctddev/solutions/mapper/IterativeParallelism.java
+++ java/ru/ifmo/ctddev/solutions/mapper/IterativeParallelism.java
@@ -1,21 +1,31 @@
-package ru.ifmo.ctddev.solutions.concurrent;
+package ru.ifmo.ctddev.solutions.mapper;
 
 import info.kgeorgiy.java.advanced.concurrent.ListIP;
+import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class IterativeParallelism implements ListIP {
 
+    private ParallelMapper parallelMapper;
+
+    public IterativeParallelism(ParallelMapper parallelMapper) {
+        this.parallelMapper = parallelMapper;
+    }
+
     @Override
     public String join(int i, List<?> list) throws InterruptedException {
         Function<List<?>, String> func = l -> l.stream()
                 .map(Object::toString)
                 .collect(Collectors.joining(""));
-        List<String> strings = func(i, list, func);
-
+        List<List<?>> listList = subLister(i, list);
+        List<String> strings = parallelMapper.map(func, listList);
         return strings.stream()
                 .map(Object::toString)
                 .collect(Collectors.joining(""));
@@ -25,7 +35,9 @@ public class IterativeParallelism implements ListIP {
     public <T> List<T> filter(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
         List<T> resultList = new ArrayList<>();
         Function<List<? extends T>, List<T>> func = l -> l.stream().filter(predicate).collect(Collectors.toList());
-        List<List<T>> lists = func(i, list, func);
+
+        List<List<? extends T>> listList = subLister(i, list);
+        List<List<T>> lists = parallelMapper.map(func, listList);
         for (List<T> listU : lists) {
             resultList.addAll(listU);
         }
@@ -36,7 +48,8 @@ public class IterativeParallelism implements ListIP {
     public <T, U> List<U> map(int i, List<? extends T> list, Function<? super T, ? extends U> function) throws InterruptedException {
         List<U> resultList = new ArrayList<>();
         Function<List<? extends T>, List<U>> func = l -> l.stream().map(s -> function.apply(s)).collect(Collectors.toList());
-        List<List<U>> lists = func(i, list, func);
+        List<List<? extends T>> listList = subLister(i, list);
+        List<List<U>> lists = parallelMapper.map(func, listList);
         for (List<U> listU : lists) {
             resultList.addAll(listU);
         }
@@ -46,20 +59,24 @@ public class IterativeParallelism implements ListIP {
     @Override
     public <T> T maximum(int i, List<? extends T> list, Comparator<? super T> comparator) throws InterruptedException {
         Function<List<? extends T>, T> func = l -> Collections.max(l, comparator);
-        List<T> listMax = func(i, list, func);
-        return Collections.max(listMax, comparator);
+        List<List<? extends T>> listList = subLister(i, list);
+
+        return Collections.max(parallelMapper.map(func, listList), comparator);
     }
 
     @Override
     public <T> T minimum(int i, List<? extends T> list, Comparator<? super T> comparator) throws InterruptedException {
         Function<List<? extends T>, T> func = l -> Collections.min(l, comparator);
-        return Collections.min(func(i, list, func), comparator);
+        List<List<? extends T>> listList = subLister(i, list);
+
+        return Collections.min(parallelMapper.map(func, listList), comparator);
     }
 
     @Override
     public <T> boolean all(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
         Function<List<? extends T>, Boolean> func = l -> l.stream().allMatch(predicate);
-        List<Boolean> listBoolean = func(i, list, func);
+        List<List<? extends T>> listList = subLister(i, list);
+        List<Boolean> listBoolean = parallelMapper.map(func, listList);
         for (Boolean b : listBoolean) {
             if (!b) return false;
         }
@@ -69,44 +86,33 @@ public class IterativeParallelism implements ListIP {
     @Override
     public <T> boolean any(int i, List<? extends T> list, Predicate<? super T> predicate) throws InterruptedException {
         Function<List<? extends T>, Boolean> func = l -> l.stream().anyMatch(predicate);
-        List<Boolean> listBoolean = func(i, list, func);
+        List<List<? extends T>> listList = subLister(i, list);
+        List<Boolean> listBoolean = parallelMapper.map(func, listList);
         for (Boolean b : listBoolean) {
             if (b) return true;
         }
         return false;
     }
 
-    private <T, R> List<R> func(int threadCounter, List<? extends T> list, Function<List<? extends  T>, R> f) throws InterruptedException {
+    private <T> List<List<? extends T>> subLister(int threadCounter, List<? extends T> list) throws InterruptedException {
         if (threadCounter > list.size()) {
             threadCounter = list.size() / 5 + 1;
         }
-        R[] results = (R[]) new Object[threadCounter];
 
-        List<Thread> threads = new ArrayList<>();
+        List<List<? extends T>> results = new ArrayList<>();
 
         int subListSize = list.size() / threadCounter;
         for (int i = 0; i < threadCounter; i++) {
             int threadCount = threadCounter;
             int threadIndex = i;
 
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    if (threadCount - 1 != threadIndex) {
-                        results[threadIndex] = f.apply(list.subList(subListSize * threadIndex, subListSize * (threadIndex + 1)));
-                    } else {
-                        results[threadIndex] = f.apply(list.subList(subListSize * threadIndex, list.size()));
-                    }
-                }
-            });
-            threads.add(thread);
-            thread.start();
-        }
-        for (Thread thread : threads) {
-            thread.join();
+            if (threadCount - 1 != threadIndex) {
+                results.add(list.subList(subListSize * threadIndex, subListSize * (threadIndex + 1)));
+            } else {
+                results.add(list.subList(subListSize * threadIndex, list.size()));
+            }
         }
 
-        return Arrays.asList(results);
+        return results;
     }
 }
-
--- java/ru/ifmo/ctddev/solutions/mapper/ParallelMapperImpl.java
+++ java/ru/ifmo/ctddev/solutions/mapper/ParallelMapperImpl.java
@@ -1,20 +1,56 @@
 package ru.ifmo.ctddev.solutions.mapper;
 
-import java.util.List;
-import java.util.function.Function;
-
 import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Function;
 
 public class ParallelMapperImpl implements ParallelMapper {
 
+    private final Lock lock = new ReentrantLock(true);
+    private ExecutorService executorService;
+
+    public ParallelMapperImpl(int threadCount) {
+        executorService =  Executors.newFixedThreadPool(threadCount > 1 ? threadCount : 1);
+    }
+
     @Override
-    public <T, R> List<R> map(Function<? super T, ? extends R> f, List<? extends T> args) throws InterruptedException {
-        //todo
-        return null;
+    public <T, R> List<R> map(Function<? super T, ? extends R> function, List<? extends T> list) throws InterruptedException {
+        lock.lock();
+        try{
+            List<Future<R>> futureList = new ArrayList<>();
+
+            for (int i = 0; i < list.size(); i++) {
+                int index = i;
+
+                futureList.add(executorService.submit(new Callable<R>() {
+                    @Override
+                    public R call() throws Exception {
+                        return function.apply(list.get(index));
+                    }
+                }));
+            }
+            List<R> result = new ArrayList<>();
+
+            for (Future<R> future : futureList) {
+                result.add(future.get());
+            }
+            return result;
+
+        } catch (ExecutionException e) {
+            throw new IllegalArgumentException(e);
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public void close() throws InterruptedException {
-        //todo
+        executorService.shutdownNow();
     }
 }
+
