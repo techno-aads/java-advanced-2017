dc76216ef1dd713684a08e8027dc2cb2a318eaef
sdkuzmin <kuzia95@bk.ru>
2018-01-09 23:02:21 +0300

    Lab 7 - fixed

--- java/ru/ifmo/ctddev/solutions/mapper/ParallelMapperImpl.java
+++ java/ru/ifmo/ctddev/solutions/mapper/ParallelMapperImpl.java
@@ -1,22 +1,25 @@
 package ru.ifmo.ctddev.solutions.mapper;
 
+import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
 
 public class ParallelMapperImpl implements ParallelMapper {
     private ExecutorService executor;
     private int threads;
     private Lock locker = new ReentrantLock(true);
 
-    public ParallelMapperImpl (int threads){
-        int maxThread =  Runtime.getRuntime().availableProcessors();
+    public ParallelMapperImpl(int threads) {
+        int maxThread = Runtime.getRuntime().availableProcessors();
         this.threads = Math.min(maxThread, threads);
 
         executor = Executors.newWorkStealingPool(this.threads);
@@ -25,35 +28,34 @@ public class ParallelMapperImpl implements ParallelMapper {
     @Override
     public <T, R> List<R> map(Function<? super T, ? extends R> f, List<? extends T> args) throws InterruptedException {
         //todo
+        //
+        ArrayList<Future<List<R>>> futures = new ArrayList<>();
         locker.lock();
-        try{
-            ArrayList<Future<List<R>>> futures = new ArrayList<>();
+        try {
             int sizeSubList = args.size() / threads;
-            for (int i =0; i < threads; i++) {
+            for (int i = 0; i < threads; i++) {
                 int localI = i;
-                Future<List<R>> future = executor.submit( ()->{
+                Future<List<R>> future = executor.submit(() -> {
                     int startIndex = sizeSubList * localI;
                     int endIndex = (localI == threads - 1) ? args.size() : startIndex + sizeSubList;
-                    return args.subList(startIndex,endIndex).stream().map(f).collect(Collectors.toList());
+                    return args.subList(startIndex, endIndex).stream().map(f).collect(Collectors.toList());
                 });
                 futures.add(future);
             }
-
-            ArrayList<R> results = new ArrayList<>();
-            for (Future<List<R>> future : futures) {
-                try {
-                    results.addAll(future.get());
-                }
-                catch (ExecutionException ex){
-                    throw new InterruptedException(ex.getMessage());
-                }
-            }
-
-            return results;
-        }
-        finally {
+        } finally {
             locker.unlock();
         }
+
+        ArrayList<R> results = new ArrayList<>();
+        for (Future<List<R>> future : futures) {
+            try {
+                results.addAll(future.get());
+            } catch (ExecutionException ex) {
+                throw new InterruptedException(ex.getMessage());
+            }
+        }
+
+        return results;
     }
 
     @Override
