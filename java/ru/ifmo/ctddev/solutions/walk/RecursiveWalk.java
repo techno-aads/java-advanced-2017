ad4f79f55aaf9138fdf71935af2367558dbb717d
zrazzhivin <zrazzhivin@gmail.com>
2018-01-09 16:57:24 +0300

    Razzhivin Zakhar 1 and 5 (#16)

--- java/ru/ifmo/ctddev/solutions/walk/RecursiveWalk.java
+++ java/ru/ifmo/ctddev/solutions/walk/RecursiveWalk.java
@@ -1,5 +1,65 @@
 package ru.ifmo.ctddev.solutions.walk;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/**
+ * @author zakhar.razzhivin
+ */
 public class RecursiveWalk {
-    //todo
+
+    private static final String HASH_STUB = "00000000";
+    private static StringBuilder outputContent;
+
+    public static void main(String[] args) {
+        try (Stream<String> stream = Files.lines(Paths.get(args[0]))) {
+            outputContent = new StringBuilder();
+            stream.forEach(RecursiveWalk::recursiveWalk);
+            Files.write(Paths.get(args[1]), outputContent.toString().getBytes(StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void recursiveWalk(String path) {
+        try {
+            Files.walk(Paths.get(path))
+                    .filter(file -> {
+                        try {
+                            return Files.isRegularFile(file);
+                        } catch (SecurityException e) {
+                            return false;
+                        }
+                    })
+                    .forEach(RecursiveWalk::calculateFNVHash);
+        } catch (Exception e) {
+            outputContent.append(HASH_STUB).append(" ").append(path).append("\n");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void calculateFNVHash(Path path) {
+        int hash = 0x811c9dc5;
+        try(FileChannel fileChannel = new FileInputStream(path.toString()).getChannel()) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            while (fileChannel.read(buffer) > 0) {
+                buffer.flip();
+                for (int i = 0; i < buffer.limit(); ++i) {
+                    hash = (hash * 0x01000193) ^ (buffer.get() & 0xff);
+                }
+                buffer.clear();
+            }
+            outputContent.append(String.format("%08X", hash).toLowerCase()).append(" ").append(path).append("\n");
+        } catch (Exception e) {
+            outputContent.append(HASH_STUB).append(" ").append(path).append("\n");
+            System.out.println(e.getMessage());
+        }
+    }
 }
