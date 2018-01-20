ec1022c4120ffc14ffd538efec413a9ffd3e82e9
ol9shvaleva <ol9shvaleva@mail.ru>
2018-01-20 14:13:22 +0300

    implemented lab 9

--- java/ru/ifmo/ctddev/solutions/hello/HelloUDPClient.java
+++ java/ru/ifmo/ctddev/solutions/hello/HelloUDPClient.java
@@ -1,5 +1,69 @@
 package ru.ifmo.ctddev.solutions.hello;
 
-public class HelloUDPClient {
-    //todo
+import info.kgeorgiy.java.advanced.hello.HelloClient;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloUDPClient implements HelloClient {
+
+    private static final int HELLO_LENGTH = 8;
+
+    @Override
+    public void run(String host, int port, String prefRequest, int requestCountInThread, int threadCount) {
+        List<Thread> threadList = new ArrayList<>(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            int threadCounter = i;
+
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try (DatagramSocket datagramSocket = new DatagramSocket()) {
+                        int requestCounter = 0;
+                        while (requestCounter < requestCountInThread) {
+                            try {
+                                datagramSocket.setSoTimeout(100);
+                                String request = prefRequest + threadCounter + "_" + requestCounter;
+                                DatagramPacket datagramPacketRequest = new DatagramPacket(request.getBytes(StandardCharsets.UTF_8),
+                                        request.getBytes().length, new InetSocketAddress(host, port));
+                                datagramSocket.send(datagramPacketRequest);
+
+                                DatagramPacket datagramPacketResponse = new DatagramPacket(
+                                        new byte[request.getBytes().length + HELLO_LENGTH],
+                                        0,
+                                        request.getBytes().length + HELLO_LENGTH);
+
+                                datagramSocket.receive(datagramPacketResponse);
+
+                                String response = new String(datagramPacketResponse.getData(), 0,
+                                        datagramPacketResponse.getLength(), StandardCharsets.UTF_8);
+
+                                if (("Hello, " + request).equals(response)) {
+                                    requestCounter++;
+                                }
+                            } catch (IOException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        }
+                    } catch (SocketException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            });
+
+            thread.start();
+            threadList.add(thread);
+        }
+
+        for (Thread thread : threadList) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 }
--- java/ru/ifmo/ctddev/solutions/hello/HelloUDPServer.java
+++ java/ru/ifmo/ctddev/solutions/hello/HelloUDPServer.java
@@ -1,5 +1,65 @@
 package ru.ifmo.ctddev.solutions.hello;
 
-public class HelloUDPServer {
-    //todo
+import info.kgeorgiy.java.advanced.hello.HelloServer;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HelloUDPServer implements HelloServer {
+
+    private final List<DatagramSocket> sockets = new ArrayList<>();
+    private final Map<Integer, ExecutorService> executors = new ConcurrentHashMap<>();
+
+    @Override
+    public void start(int port, int threadCount) {
+        executors.putIfAbsent(port, Executors.newFixedThreadPool(threadCount));
+
+        DatagramSocket socket;
+        try {
+            socket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            throw new IllegalArgumentException("Can not create socket");
+        }
+        sockets.add(socket);
+
+        executors.get(port).submit(() -> {
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    socket.setSoTimeout(100);
+                    DatagramPacket datagramPacketRequest = new DatagramPacket(
+                            new byte[socket.getReceiveBufferSize()],
+                            socket.getReceiveBufferSize());
+                    socket.receive(datagramPacketRequest);
+                    String response = "Hello, " + new String(
+                            datagramPacketRequest.getData(),
+                            0,
+                            datagramPacketRequest.getLength(),
+                            StandardCharsets.UTF_8);
+                    InetAddress IPAddress = datagramPacketRequest.getAddress();
+                    int receivePort = datagramPacketRequest.getPort();
+                    DatagramPacket datagramPacketResponse = new DatagramPacket(response.getBytes(
+                            StandardCharsets.UTF_8), response.getBytes().length, IPAddress, receivePort);
+                    socket.send(datagramPacketResponse);
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        });
+    }
+
+    @Override
+    public void close() {
+        executors.values().forEach(ExecutorService::shutdownNow);
+        sockets.forEach(DatagramSocket::close);
+    }
 }
