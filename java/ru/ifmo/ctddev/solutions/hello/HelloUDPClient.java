266ff1f500640dc9321d69c46eadbeb5d862e80e
ladatrimasova <acanehoshi@mail.ru>
2018-01-20 18:59:45 +0300

    HelloUdp - lab 9

--- java/ru/ifmo/ctddev/solutions/hello/HelloUDPClient.java
+++ java/ru/ifmo/ctddev/solutions/hello/HelloUDPClient.java
@@ -1,5 +1,94 @@
 package ru.ifmo.ctddev.solutions.hello;
 
-public class HelloUDPClient {
-    //todo
+import info.kgeorgiy.java.advanced.hello.HelloClient;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.Charset;
+
+/**
+ * A class, created UDP Client, which send packages to specified host and port
+ */
+public class HelloUDPClient implements HelloClient {
+    private static final int SOCKET_TIMEOUT = 200;
+
+    /**
+     * Start client on args[0] host, args[1] port, sent args[2] prefix in args[4] threads args[3] times
+     *
+     * @param args array of command line args: args[0] host, args[1] port, sent args[2] prefix in args[4] threads args[3] times
+     */
+    public static void main(String[] args) {
+        HelloUDPClient udpClient = new HelloUDPClient();
+        udpClient.run(args[0], Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), Integer.parseInt(args[4]));
+    }
+
+    private String readResponse(DatagramPacket receivePacket) {
+        return new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength(), Charset.forName("UTF-8"));
+    }
+
+    /**
+     * Start UDP packages sender
+     *
+     * @param host     host send to
+     * @param port     port send to
+     * @param prefix   string to send
+     * @param requests count times
+     * @param threads  in count threads
+     */
+    @Override
+    public void run(String host, int port, String prefix, int requests, int threads) {
+        Thread[] pool = new Thread[threads];
+        final InetAddress serverAddress;
+        try {
+            serverAddress = InetAddress.getByName(host);
+        } catch (UnknownHostException e) {
+            System.out.println("Unknown host: " + host + "\n" + e.getMessage());
+            return;
+        }
+        for (int i = 0; i < pool.length; i++) {
+            final int threadNumber = i;
+            pool[i] = new Thread(() -> {
+                try {
+                    DatagramSocket socket = new DatagramSocket();
+                    byte[] responseBuffer = new byte[socket.getReceiveBufferSize()];
+                    socket.setSoTimeout(SOCKET_TIMEOUT);
+                    for (int j = 0; j < requests; j++) {
+                        String sendRequest = prefix.concat(String.valueOf(threadNumber))
+                                .concat("_").concat(String.valueOf(j));
+                        byte[] bytes = sendRequest.getBytes(Charset.forName("UTF-8"));
+                        DatagramPacket requestPacket = new DatagramPacket(bytes, bytes.length, serverAddress, port);
+                        socket.send(requestPacket);
+                        DatagramPacket responsePacket = new DatagramPacket(responseBuffer, responseBuffer.length);
+                        String response = "";
+                        while (true) {
+                            try {
+                                socket.receive(responsePacket);
+                                response = readResponse(responsePacket);
+                                if (response.equals("Hello, " + sendRequest)) {
+                                    break;
+                                }
+                            } catch (SocketTimeoutException e) {
+                                socket.send(requestPacket);
+                                System.out.println("Request " + sendRequest + " time outed, resending");
+                            }
+                        }
+                        System.out.println(">> " + sendRequest + "\n<< " + response + "\n");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        for (Thread t : pool) {
+            t.start();
+        }
+        try {
+            for (Thread t : pool) {
+                t.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- java/ru/ifmo/ctddev/solutions/hello/HelloUDPServer.java
+++ java/ru/ifmo/ctddev/solutions/hello/HelloUDPServer.java
@@ -1,5 +1,118 @@
 package ru.ifmo.ctddev.solutions.hello;
 
-public class HelloUDPServer {
-    //todo
+import info.kgeorgiy.java.advanced.hello.HelloServer;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+/**
+ * A class, created Server, UDP Sockets listener on localhost and specified ports
+ */
+public class HelloUDPServer implements HelloServer {
+
+    private ArrayList<Server> servers;
+
+    /**
+     * Default constructor
+     */
+    public HelloUDPServer() {
+        servers = new ArrayList<>();
+    }
+
+    /**
+     * Start server on args[0] ports, using args[1] threads to process UDP packages
+     *
+     * @param args array of command line args: args[0] - port, args[1] threads count
+     */
+    public static void main(String[] args) {
+        HelloUDPServer udpServer = new HelloUDPServer();
+        udpServer.start(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+    }
+
+    /**
+     * Start listener server
+     *
+     * @param port    port listen to
+     * @param threads number of  threads run in
+     */
+    @Override
+    public void start(int port, int threads) {
+        Server server = null;
+        try {
+            server = new Server(port, threads);
+        } catch (SocketException e) {
+            System.out.println("Can't open socket on port: " + port + "\n" + e.getMessage());
+            return;
+        }
+        servers.add(server);
+        server.start();
+    }
+
+    /**
+     * Interrupt all started in start() threads
+     */
+    @Override
+    public void close() {
+        for (Server server : servers) {
+            server.stop();
+        }
+    }
+
+    private class Server {
+        final private DatagramSocket socket;
+        private Thread[] pool;
+
+        public Server(int port, int threads) throws SocketException {
+            socket = new DatagramSocket(port);
+            this.pool = new Thread[threads];
+            for (int i = 0; i < threads; i++) {
+                pool[i] = new Thread(() -> {
+                    try {
+                        byte[] buffer = new byte[socket.getReceiveBufferSize()];
+                        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                        while (true) {
+                            if (Thread.currentThread().isInterrupted()) {
+                                return;
+                            }
+                            try {
+                                socket.receive(packet);
+                                String message = getResponseMessage(packet);
+                                InetAddress address = packet.getAddress();
+                                int toPort = packet.getPort();
+                                sendRequest(socket, "Hello, " + message, address, toPort);
+                            } catch (SocketTimeoutException e) {
+                                System.err.println(e.getMessage());
+                            }
+                        }
+                    } catch (IOException ignored) {
+                    }
+                });
+            }
+        }
+
+        private void sendRequest(DatagramSocket socket, String request, InetAddress address, int port) throws IOException {
+            byte[] bytes = request.getBytes(Charset.forName("UTF-8"));
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, port);
+            socket.send(packet);
+        }
+
+        private String getResponseMessage(DatagramPacket receivePacket) {
+            return new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength(), Charset.forName("UTF-8"));
+        }
+
+        public void start() {
+            for (Thread t : pool) {
+                t.start();
+            }
+        }
+
+        public void stop() {
+            for (Thread t : pool) {
+                t.interrupt();
+            }
+            socket.close();
+        }
+    }
 }
