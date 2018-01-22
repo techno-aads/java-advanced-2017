272d617e4ed8b1d903eb5bdc391cd3633da5dec0
BlitzAL <34640386+BlitzAL@users.noreply.github.com>
2018-01-22 22:16:27 +0300

    Update ArraySet.java

--- java/ru/ifmo/ctddev/solutions/arrayset/ArraySet.java
+++ java/ru/ifmo/ctddev/solutions/arrayset/ArraySet.java
@@ -1,316 +1,347 @@
 package ru.ifmo.ctddev.solutions.arrayset;
 
+
 import java.lang.reflect.Array;
 import java.util.*;
 
-public class ArraySet<E> extends AbstractSet<E> implements NavigableSet<E>{
+public class ArraySet<E> extends AbstractSet<E> implements NavigableSet<E> {
 
-    List<E> data;
-    List<E> dataReversed;
+    List<E> list;
+    List<E> revlist;
     Comparator<? super E> comparator;
-    boolean defaultOrder = false;
+    boolean value = false;
 
-    public ArraySet(Collection<E> collection, Comparator<? super E> comparator){
-        this.comparator = comparator;
-        ArrayList<E> inputData = new ArrayList<>(collection);
-        ArrayList<E> tempData = new ArrayList<>(inputData.size());
-        inputData.sort(comparator);
-        if(inputData.size() != 0){
-            tempData.add(inputData.get(0));
-        }
-        for(int i = 1; i< inputData.size();i++){
-            if (comparator.compare(inputData.get(i), inputData.get(i - 1)) != 0) {
-                tempData.add(inputData.get(i));
-            }
-        }
-        tempData.trimToSize();
-        data = tempData;
-        dataReversed = new ArrayList<>(data);
-        dataReversed.sort(comparator.reversed());
-    }
-
-    public ArraySet(Collection<E> collection){
-        this(collection, (o1, o2) -> ((Comparable<E>)o1).compareTo(o2));
-        defaultOrder = true;
-    }
-
-    public ArraySet(){
-        data = new ArrayList<>();
-        dataReversed = new ArrayList<>();
+    public ArraySet() {
+        list = new ArrayList<>();
+        revlist = new ArrayList<>();
         comparator = (Comparator<E>) (o1, o2) -> 0;
-        defaultOrder = true;
+        value = true;
     }
 
-    public ArraySet(List<E> data, List<E> dataReversed, Comparator<? super E> comparator, boolean defaultOrder ){
-        this.data = data;
-        this.dataReversed = dataReversed;
-        this.comparator = comparator;
-        this.defaultOrder = defaultOrder;
+    public ArraySet(Collection<E> collection0, Comparator<? super E> comparator0) {
+        comparator = comparator0;
+        list = addCollection(new ArrayList<>(collection0),comparator);
+        revlist = new ArrayList<>(list);
+        revlist.sort(comparator0.reversed());
     }
 
-    private int indexOf(E item){
-
-        return Collections.binarySearch(data, item, comparator);
-
+    private List<E> addCollection(List<E> elements, Comparator<? super E> comparator) {
+        List<E> uniqueElements = new ArrayList<>();
+        Set<E> unique = new TreeSet<E>(comparator);
+        unique.addAll(elements);
+        uniqueElements.addAll(unique);
+        return uniqueElements;
     }
 
+    public ArraySet(Collection<E> collection) {
+        this(collection, (obj1, obj2) -> ((Comparable<E>)obj1).compareTo(obj2));
+        value = true;
+    }
 
-    // ----------------------------------------
+    public ArraySet(List<E> list0, List<E> revlist0, Comparator<? super E> comparator0, boolean value0) {
+        list = list0;
+        revlist = revlist0;
+        comparator = comparator0;
+        value = value0;
+       }
+
+    private int indexOf(E a) {
+        int Vreturn = Collections.binarySearch(list, a, comparator);
+        return Vreturn;
+        //return Collections.binarySearch(data, item, comparator);
+    }
 
     @Override
-    public E lower(E item) {
-        int index = indexOf(item);
-        if((index != 0)&&(index != -1)){
-
-            if(index < 0){
-                index = -(index + 1);
-            }
-
-            return data.get(index - 1);
-
-        }else{
-            return null;
+    public E ceiling(E a) {
+        int i = indexOf(a);
+        E Vreturn;
+        if(i >= 0) {
+            Vreturn = list.get(i);
+            //return data.get(index);
         }
-
-
-    }
-
-    @Override
-    public E ceiling(E item) {
-        int index = indexOf(item);
-        if(index >= 0){
-
-            return data.get(index);
-        }else{
-            index = -(index + 1);
-
-            if(index == data.size()){
-                return null;
-            }else{
-                return data.get(index);
+        else {
+            i = -(i + 1);
+            if(i == list.size()) {
+                Vreturn = null;
+                //return null;
             }
-
-        }
-
-
-    }
-
-    @Override
-    public E higher(E item) {
-        int index = indexOf(item);
-        if(index >= 0){
-            if(index == data.size() - 1){
-                return null;
-            }else{
-                return data.get(index + 1);
-            }
-        }else{
-            index = -(index + 1);
-            if(index == data.size()){
-                return null;
-            }else{
-                return data.get(index);
+            else {
+                Vreturn = list.get(i);
+                //return data.get(index);
             }
         }
+        return Vreturn;
     }
 
     @Override
-    public E floor(E item) {
-        int index = indexOf(item);
-        if(index >= 0){
-
-            return data.get(index);
-
-        }else{
-            index = -(index + 1);
-            if(index == 0){
-                return null;
-            }else{
-                return data.get(index - 1);
+    public E floor(E a) {
+        int i = indexOf(a);
+        E Vreturn;
+        if(i >= 0) {
+            Vreturn = list.get(i);
+            //return data.get(index);
+        }
+        else{
+            i = -(i + 1);
+            if(i == 0) {
+                Vreturn = null;
+            }
+            else {
+                Vreturn = list.get(i - 1);
             }
         }
+        return Vreturn;
     }
 
     @Override
-    public SortedSet<E> tailSet(E fromElement) {
-        return tailSet(fromElement, true);
+    public E lower(E a) {
+        int i = indexOf(a);
+        E Vreturn;
+        if((i != 0) && (i != -1)) {
+            if(i < 0) {
+                i = -(i + 1);
+            }
+            Vreturn = list.get(i - 1);
+        }
+        else {
+            Vreturn = null;
+        }
+        return Vreturn;
     }
 
     @Override
-    public NavigableSet<E> tailSet(E fromElement, boolean inclusive) {
-        int index = indexOf(fromElement);
-
-        if(index < 0){
-            index = -(index + 1);
-        }else{
-            if(!inclusive){
-                index += 1;
+    public E higher(E a) {
+        int i = indexOf(a);
+        E Vreturn;
+        if(i >= 0) {
+            if(i == list.size() - 1) {
+                Vreturn = null;
+                //return null;
+            }
+            else {
+                Vreturn = list.get(i + 1);
+                //return data.get(index + 1);
             }
         }
-
-        return new ArraySet<E>(data.subList(index, size()), dataReversed.subList(0, size() - index),  this.comparator, defaultOrder);
-    }
-
-    @Override
-    public SortedSet<E> headSet(E toElement) {
-        return headSet(toElement, false);
-    }
-
-    @Override
-    public NavigableSet<E> headSet(E toElement, boolean inclusive) {
-        int index = indexOf(toElement);
-
-        if(index < 0){
-            index = -(index + 1);
-        }else{
-            if(inclusive){
-                index += 1;
+        else {
+            i = -(i + 1);
+            if(i == list.size()) {
+                Vreturn = null;
+                //return null;
+            }
+            else {
+                Vreturn = list.get(i);
+                //return data.get(index);
             }
         }
-
-        return new ArraySet<E>(data.subList(0, index), dataReversed.subList(size() - index, size()), this.comparator, this.defaultOrder);
+        return Vreturn;
     }
 
     @Override
-    public NavigableSet<E> subSet(E fromElement, boolean fromInclusive, E toElement, boolean toInclusive) {
-        return headSet(toElement, toInclusive).tailSet(fromElement, fromInclusive);
+    public SortedSet<E> tailSet(E from_elem) {
+        SortedSet<E> Vreturn = tailSet(from_elem, true);
+        return Vreturn;
     }
 
     @Override
-    public SortedSet<E> subSet(E fromElement, E toElement) {
-        return subSet(fromElement, true, toElement, false);
+    public SortedSet<E> headSet(E to_elem) {
+        SortedSet<E> Vreturn = headSet(to_elem, false);
+        return Vreturn;
+     }
+
+    @Override
+    public SortedSet<E> subSet(E from_elem, E to_elem) {
+        SortedSet<E> Vreturn = subSet(from_elem, true, to_elem, false);
+        return Vreturn;
     }
+
+    @Override
+    public NavigableSet<E> tailSet(E from_elem, boolean flag_incl) {
+        int i = indexOf(from_elem);
+        if(i < 0) {
+            i = -(i + 1);
+        }
+        else{
+            if(flag_incl == false) {
+                i += 1;
+            }
+        }
+        return new ArraySet<E>(list.subList(i, size()), revlist.subList(0, size() - i), comparator, value);
+      }
+
+    @Override
+    public NavigableSet<E> headSet(E to_elem, boolean flag_incl) {
+        int i = indexOf(to_elem);
+        if(i < 0) {
+            i = -(i + 1);
+        }
+        else {
+            if(flag_incl == true) {
+                i += 1;
+            }
+        }
+        return new ArraySet<E>(list.subList(0, i), revlist.subList(size() - i, size()), comparator, value);
+       }
+
+    @Override
+    public NavigableSet<E> subSet(E from_elem, boolean flag_incl_from, E to_elem, boolean flag_incl_to) {
+        NavigableSet<E> Vreturn = headSet(to_elem, flag_incl_to).tailSet(from_elem, flag_incl_from);
+        return Vreturn;
+    }
+
+    @Override
+    public NavigableSet<E> descendingSet() {
+        return new ArraySet<E>(revlist, list, Collections.reverseOrder(comparator), false);
+     }
 
     @Override
     public E pollFirst() {
-        throw new UnsupportedOperationException("nenado");
+        throw new UnsupportedOperationException("Unsupported operation");
     }
 
     @Override
     public E pollLast() {
-        throw new UnsupportedOperationException("nenado");
+        throw new UnsupportedOperationException("Unsupported operation");
     }
 
     @Override
     public int size() {
-        return data.size();
+        int Vreturn = list.size();
+        return Vreturn;
     }
 
     @Override
     public boolean isEmpty() {
-        return data.isEmpty();
+        boolean Vreturn = list.isEmpty();
+        return Vreturn;
     }
 
     @Override
-    public boolean contains(Object o) {
-        return Collections.binarySearch(data, (E) o, comparator) >= 0;
+    public boolean contains(Object obj) {
+
+        boolean Vreturn = true;
+        if (Collections.binarySearch(list, (E) obj, comparator) < 0) {
+            Vreturn = false;
+        }
+        return Vreturn;
     }
 
-    @Override
     public Iterator<E> iterator() {
-        return new Iterator<E>() {
-            Iterator<E> it = data.iterator();
+
+       return new Iterator<E>() {
+            Iterator<E> iter = list.iterator();
 
             @Override
             public boolean hasNext() {
-                return it.hasNext();
+                boolean Vreturn = iter.hasNext();
+                return Vreturn;
             }
 
             @Override
             public E next() {
-                return it.next();
-            }
-
-            @Override
-            public void remove() {
-                throw new UnsupportedOperationException();
+                E Vreturn = iter.next();
+                return Vreturn;
             }
         };
     }
 
     @Override
     public boolean add(E e) {
-        throw new UnsupportedOperationException("nenado");
+        throw new UnsupportedOperationException("Unsupported operation");
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        throw new UnsupportedOperationException("Unsupported operation");
     }
 
     @Override
     public boolean remove(Object o) {
-        throw new UnsupportedOperationException("nenado");
-    }
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        throw new UnsupportedOperationException("nenado");
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        throw new UnsupportedOperationException("nenado");
+        throw new UnsupportedOperationException("Unsupported operation");
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        throw new UnsupportedOperationException("nenado");
+        throw new UnsupportedOperationException("Unsupported operation");
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Unsupported operation");
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException("nenado");
-    }
-
-    @Override
-    public NavigableSet<E> descendingSet() {
-        return new ArraySet<E>(this.dataReversed, this.data, Collections.reverseOrder(comparator), false);
+        throw new UnsupportedOperationException("Unsupported operation");
     }
 
     @Override
     public Iterator<E> descendingIterator() {
-        return descendingSet().iterator();
+        Iterator<E> Vreturn = descendingSet().iterator();
+        return Vreturn;
+        //return descendingSet().iterator();
     }
 
     @Override
     public Comparator<? super E> comparator() {
-        if(defaultOrder){
-            return null;
-        }else{
-            return comparator;
+        Comparator<? super E> Vreturn;
+        if(value == true) {
+            Vreturn = null;
+            //return null;
         }
+        else {
+            Vreturn = comparator;
+            //return comparator;
+        }
+        return Vreturn;
     }
 
     @Override
     public Object[] toArray() {
-        return data.toArray();
+        Object[] Vreturn = list.toArray();
+        return Vreturn;
     }
 
     @Override
-    public <T> T[] toArray(T[] a) {
-
-        T[] arr = (T[])(new Object[data.size()]);
-        Object[] dd = data.toArray();
-
-        for(int i = 0; i<data.size(); i++){
-            arr[i] = (T) dd[i];
+    public <T> T[] toArray(T[] t) {
+        T[] array = (T[])(new Object[list.size()]);
+        Object[] obj = list.toArray();
+        for(int i = 0; i < list.size(); i++) {
+            array[i] = (T) obj[i];
         }
-        return arr;
+        return array;
     }
 
     @Override
     public E first() {
-        if (data.size() != 0)
-            return data.get(0);
-        else
+        E Vreturn;
+        if (list.size() != 0) {
+            Vreturn = list.get(0);
+            return Vreturn;
+            //return data.get(0);
+        }
+        else {
             throw new NoSuchElementException();
+        }
     }
 
     @Override
     public E last() {
-        if (data.size() != 0)
-            return data.get(data.size() - 1);
-        else
+        E Vreturn;
+        if (list.size() != 0) {
+            Vreturn = list.get(list.size() - 1);
+            return Vreturn;
+            //return data.get(data.size() - 1);
+        }
+        else {
             throw new NoSuchElementException();
+        }
     }
 
-    public String toString(){
-        return data.toString();
+    public String toString() {
+        String Vreturn = list.toString();
+        return Vreturn;
+        //return data.toString();
     }
+
 }
-
