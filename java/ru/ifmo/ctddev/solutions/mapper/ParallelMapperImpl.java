0823380685a588e1d86c3c51e5ec6033768ed149
LilyaZmievets <35195422+LilyaZmievets@users.noreply.github.com>
2018-02-09 12:03:16 +0300

    Update ParallelMapperImpl.java
    Lab7

--- java/ru/ifmo/ctddev/solutions/mapper/ParallelMapperImpl.java
+++ java/ru/ifmo/ctddev/solutions/mapper/ParallelMapperImpl.java
@@ -1,20 +1,41 @@
 package ru.ifmo.ctddev.solutions.mapper;
 
-import java.util.List;
-import java.util.function.Function;
-
 import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.function.Function;
+
 public class ParallelMapperImpl implements ParallelMapper {
+    private final ExecutorService executorService;
 
-    @Override
-    public <T, R> List<R> map(Function<? super T, ? extends R> f, List<? extends T> args) throws InterruptedException {
-        //todo
-        return null;
+    public ParallelMapperImpl(int threads) {
+        executorService = Executors.newFixedThreadPool(threads);
     }
 
     @Override
-    public void close() throws InterruptedException {
-        //todo
+    public <T, R> List<R> map(Function<? super T, ? extends R> function, List<? extends T> list) throws InterruptedException {
+        List<Future<R>> tasks = new ArrayList<>(list.size());
+
+        synchronized (executorService) {
+            for (T value: list) tasks.add(executorService.submit(() -> function.apply(value)));
+        }
+
+        List<R> results = new ArrayList<>(list.size());
+        try {
+            for (Future<R> f: tasks) {
+                try { results.add(f.get()); }
+                catch (CancellationException | ExecutionException e) { results.add(null); }
+            }
+
+            return results;
+        }
+        finally {
+            if (results.size() == list.size()) for (Future<R> f : tasks) f.cancel(true);
+        }
     }
+
+    @Override
+    public void close() { executorService.shutdownNow(); }
 }
