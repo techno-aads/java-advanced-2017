2ef2ef82569a28d0a57e91d52d99a5bbe7c5b3b7
Tegark <stroggom@gmail.com>
2018-02-11 15:57:35 +0300

    Lab 6. ScalarIP

--- java/ru/ifmo/ctddev/solutions/concurrent/IterativeParallelism.java
+++ java/ru/ifmo/ctddev/solutions/concurrent/IterativeParallelism.java
@@ -1,46 +1,125 @@
 package ru.ifmo.ctddev.solutions.concurrent;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.function.Function;
+import java.util.*;
 import java.util.function.Predicate;
 
-import info.kgeorgiy.java.advanced.concurrent.ListIP;
+import info.kgeorgiy.java.advanced.concurrent.ScalarIP;
 
-public class IterativeParallelism implements ListIP {
+public class IterativeParallelism implements ScalarIP
+{
 
     @Override
-    public <T> T maximum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
-        return null;
+    public <T> T maximum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException
+    {
+        Thread[] threadPool = new Thread[threads];
+        List<T> maximums = new ArrayList<>();
+        int countThread = Integer.min(threads, values.size());
+        int sizeSubList = values.size() / countThread;
+
+        for (int i = 0; i < countThread; i++)
+        {
+            maximums.add(null);
+        }
+
+        for (int i = 0; i < countThread; i++)
+        {
+            int localID = i;
+
+            Thread thread = new Thread(() -> {
+                int startIndex = sizeSubList * localID;
+                int endIndex;
+
+                if(localID == countThread - 1)
+                {
+                    endIndex = values.size();
+                }
+                else
+                {
+                    endIndex = startIndex + sizeSubList;
+                }
+
+                T localMax = Collections.max(values.subList(startIndex, endIndex), comparator);
+
+                maximums.set(localID, localMax);
+            });
+
+            threadPool[i] = thread;
+            thread.start();
+        }
+
+        for (int i = 0; i < countThread; i++)
+        {
+            threadPool[i].join();
+        }
+
+        return Collections.max(maximums, comparator);
     }
 
     @Override
-    public <T> T minimum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
-        return null;
+    public <T> T minimum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException
+    {
+        return maximum(threads, values, Collections.reverseOrder(comparator));
     }
 
     @Override
-    public <T> boolean all(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-        return false;
+    public <T> boolean all(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException
+    {
+        Thread[] threadPool = new Thread[threads];
+        int countThread = Integer.min(threads, values.size());
+        int sizeSubList = values.size() / countThread;
+        List<Boolean> result = new ArrayList<>();
+        Object locker = new Object();
+
+        result.add(true);
+
+
+        for (int i = 0; i < countThread; i++)
+        {
+            int localI = i;
+            Thread thread = new Thread(() -> {
+
+                int startIndex = sizeSubList * localI;
+
+                int endIndex;
+                if(localI == countThread - 1)
+                {
+                    endIndex = values.size();
+                }
+                else
+                {
+                    endIndex = startIndex + sizeSubList;
+                }
+
+                //  int endIndex = (localI == countThread - 1) ? values.size() : startIndex + sizeSubList;
+
+                if (startIndex < endIndex)
+                {
+                    boolean localResult = values.subList(startIndex, endIndex).stream().allMatch(predicate);
+                    synchronized (locker)
+                    {
+                        if (!localResult)
+                        {
+                            result.set(0, false);
+                        }
+                    }
+                }
+            });
+
+            threadPool[i] = thread;
+            thread.start();
+        }
+
+        for (int i = 0; i < countThread; i++)
+        {
+            threadPool[i].join();
+        }
+
+        return result.get(0);
     }
 
     @Override
-    public <T> boolean any(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-        return false;
-    }
-
-    @Override
-    public String join(int threads, List<?> values) throws InterruptedException {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public <T> List<T> filter(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public <T, U> List<U> map(int threads, List<? extends T> values, Function<? super T, ? extends U> f) throws InterruptedException {
-        throw new UnsupportedOperationException();
+    public <T> boolean any(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException
+    {
+        return !all(threads, values, predicate.negate());
     }
 }
