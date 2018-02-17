87daadebf5a2791f18e3ab505c8abd29762d7dc6
AlexanderDyomin <dyomisasha@yandex.ru>
2018-02-17 15:37:00 +0300

    Fixed lab 5

--- java/Javadoc.bat
+++ java/Javadoc.bat
@@ -1,1 +1,1 @@
-javadoc ru/ifmo/ctddev/solutions/implementor/Implementor.java -d doc -private -link https://docs.oracle.com/javase/8/docs/api/
+javadoc ru/ifmo/ctddev/solutions/implementor/Implementor.java -d doc -private -link https://docs.oracle.com/javase/8/docs/api/ -sourcepath ./junit-4.11.jar 
