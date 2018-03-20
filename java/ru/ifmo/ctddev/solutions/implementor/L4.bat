ab5c325090f7764ad88bfb7e0bcc9cab3efbb1c8
LilyaZmievets <35195422+LilyaZmievets@users.noreply.github.com>
2018-03-20 19:57:45 +0300

    Create L4.bat

---
+++ java/ru/ifmo/ctddev/solutions/implementor/L4.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Java\jdk1.8.0_144\bin\javac.exe" d . info/kgeorgiy/java/advanced/implementor/Impler.java info/kgeorgiy/java/advanced/implementor/JarImpler.java info/kgeorgiy/java/advanced/implementor/ImplerException.java ru/ifmo/ctddev/solutions/implementor/Implementor.java
+"C:\Program Files\Java\jdk1.8.0_144\bin\jar.exe" cfm Implementor.jar Manifest.mf info/kgeorgiy/java/advanced/implementor/Impler.class info/kgeorgiy/java/advanced/implementor/JarImpler.class info/kgeorgiy/java/advanced/implementor/ImplerException.class ru/ifmo/ctddev/solutions/implementor/Implementor.class
+pause
